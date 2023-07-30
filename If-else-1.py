cc291ef32ac3fe65d09c1bbd32ae3d2ad3a847c7
SangeethaMP94 <140884105+SangeethaMP94@users.noreply.github.com>
2023-07-30 20:56:25 +0530

    Create If-else-1.py

---
+++ If-else-1.py
@@ -0,0 +1,11 @@
+n1=int(input("Akila's age: "))
+n2=int(input("Baliji's age: "))
+n3=int(input("Perumal's age: "))
+if n1>n2 and n1>n3:
+  print("Akila is older")
+elif n2>n1 and n2>n3:
+  print("Balaji is older")
+elif n3>n1 and n3>n1:
+  print("Perumal is older")
+else:
+  print("All are equal")
